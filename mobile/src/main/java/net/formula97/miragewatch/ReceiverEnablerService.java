49d8d2838881c0e223e6e8a49886a509d824f6df
HAJIME Fukuna <f97one@gmail.com>
2015-12-31 15:41:45 +0900

    BroadcastReceiverの定義場所を誤っていたのを修正。

--- mobile/src/main/java/net/formula97/miragewatch/ReceiverEnablerService.java
+++ mobile/src/main/java/net/formula97/miragewatch/ReceiverEnablerService.java
@@ -1,6 +1,8 @@
 package net.formula97.miragewatch;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 
@@ -12,5 +14,16 @@ public class ReceiverEnablerService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * バッテリー状態更新BroadcastをキャッチするためのBroadcastReceiver。
+     */
+    public class BattStatusReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+        }
     }
 }
