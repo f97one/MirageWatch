f75cbe9863c5814f64760d9675560f9f4e1a99a9
HAJIME Fukuna <f97one@gmail.com>
2015-12-31 15:32:14 +0900

    バッテリー情報永続化周りの処理を仮実装。

--- mobile/src/main/java/net/formula97/miragewatch/BattLevelCaptureService.java
+++ mobile/src/main/java/net/formula97/miragewatch/BattLevelCaptureService.java
@@ -12,14 +12,10 @@ import android.content.Context;
  * helper methods.
  */
 public class BattLevelCaptureService extends IntentService {
-    // TODO: Rename actions, choose action names that describe tasks that this
-    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-    private static final String ACTION_FOO = "net.formula97.miragewatch.action.FOO";
-    private static final String ACTION_BAZ = "net.formula97.miragewatch.action.BAZ";
 
-    // TODO: Rename parameters
-    private static final String EXTRA_PARAM1 = "net.formula97.miragewatch.extra.PARAM1";
-    private static final String EXTRA_PARAM2 = "net.formula97.miragewatch.extra.PARAM2";
+    private static final String ACTION_CAPTURE_BATTERY_STATUS = "net.formula97.miragewatch.ACTION_CAPTURE_BATTERY_STATUS";
+
+    private static final String EXTRA_BATTERY_LEVEL = "EXTRA_BATTERY_LEVEL";
 
     public BattLevelCaptureService() {
         super("BattLevelCaptureService");
@@ -31,27 +27,10 @@ public class BattLevelCaptureService extends IntentService {
      *
      * @see IntentService
      */
-    // TODO: Customize helper method
-    public static void startActionFoo(Context context, String param1, String param2) {
+    public static void startCaptureAction(Context context, int currentBatteryLevel) {
         Intent intent = new Intent(context, BattLevelCaptureService.class);
-        intent.setAction(ACTION_FOO);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
-        context.startService(intent);
-    }
-
-    /**
-     * Starts this service to perform action Baz with the given parameters. If
-     * the service is already performing a task this action will be queued.
-     *
-     * @see IntentService
-     */
-    // TODO: Customize helper method
-    public static void startActionBaz(Context context, String param1, String param2) {
-        Intent intent = new Intent(context, BattLevelCaptureService.class);
-        intent.setAction(ACTION_BAZ);
-        intent.putExtra(EXTRA_PARAM1, param1);
-        intent.putExtra(EXTRA_PARAM2, param2);
+        intent.setAction(ACTION_CAPTURE_BATTERY_STATUS);
+        intent.putExtra(EXTRA_BATTERY_LEVEL, currentBatteryLevel);
         context.startService(intent);
     }
 
@@ -59,33 +38,21 @@ public class BattLevelCaptureService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         if (intent != null) {
             final String action = intent.getAction();
-            if (ACTION_FOO.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionFoo(param1, param2);
-            } else if (ACTION_BAZ.equals(action)) {
-                final String param1 = intent.getStringExtra(EXTRA_PARAM1);
-                final String param2 = intent.getStringExtra(EXTRA_PARAM2);
-                handleActionBaz(param1, param2);
+            if (ACTION_CAPTURE_BATTERY_STATUS.equals(action)) {
+                final int battLevel = intent.getIntExtra(EXTRA_BATTERY_LEVEL, 0);
+
+                MyApplication app = (MyApplication) getApplication();
+                app.setCurrentRemainLevel(battLevel);
             }
         }
     }
 
-    /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionFoo(String param1, String param2) {
-        // TODO: Handle action Foo
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
 
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
-    private void handleActionBaz(String param1, String param2) {
-        // TODO: Handle action Baz
-        throw new UnsupportedOperationException("Not yet implemented");
+        // IntentService破棄のタイミングで永続化
+        MyApplication app = (MyApplication) getApplication();
+        app.storeBattLevel();
     }
 }
--- mobile/src/main/java/net/formula97/miragewatch/MyApplication.java
+++ mobile/src/main/java/net/formula97/miragewatch/MyApplication.java
@@ -1,6 +1,8 @@
 package net.formula97.miragewatch;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 /**
  * Created by f97one on 15/12/31.
@@ -10,10 +12,18 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        mCurrentRemainLevel = 0.0;
+        mCurrentRemainLevel = -1;
     }
 
-    private double mCurrentRemainLevel;
+    /**
+     * 現在のバッテリーレベル
+     */
+    private int mCurrentRemainLevel;
+
+    /**
+     * バッテリーレベルをPreferenceに永続化するときのキー
+     */
+    private final String mPrefKeyBatteryLevel = "mPrefKeyBatteryLevel";
 
     /**
      * 保存されている残存バッテリーレベルを取得する。
@@ -21,10 +31,30 @@ public class MyApplication extends Application {
      * @return 残存バッテリーレベル
      */
     public double getCurrentRemainLevel() {
+        // 初期値のままの場合は、Preferenceの値の取得を試みる
+        if (mCurrentRemainLevel == -1f) {
+            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+            mCurrentRemainLevel = pref.getInt(mPrefKeyBatteryLevel, 0);
+        }
+
         return mCurrentRemainLevel;
     }
 
-    public void setCurrentRemainLevel(double currentRemainLevel) {
+    /**
+     * バッテリーレベルをインメモリで保持する。
+     *
+     * @param currentRemainLevel 現在のバッテリーレベル
+     */
+    public void setCurrentRemainLevel(int currentRemainLevel) {
         mCurrentRemainLevel = currentRemainLevel;
+    }
+
+
+    public void storeBattLevel() {
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = pref.edit();
+
+        editor.putInt(mPrefKeyBatteryLevel, mCurrentRemainLevel);
+        editor.apply();
     }
 }
