0ca7dbaf78d9be21b9cdc8ef6bc7410d886f4524
HAJIME Fukuna <f97one@gmail.com>
2016-01-02 09:55:50 +0900

    Serviceシャットダウンと相互Bindについてのスタブを追加。

--- mobile/src/main/java/net/formula97/miragewatch/ReceiverEnablerService.java
+++ mobile/src/main/java/net/formula97/miragewatch/ReceiverEnablerService.java
@@ -2,27 +2,36 @@ package net.formula97.miragewatch;
 
 import android.app.Service;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.BatteryManager;
+import android.os.Binder;
 import android.os.IBinder;
 import android.text.TextUtils;
 
 public class ReceiverEnablerService extends Service {
-    public ReceiverEnablerService() {
-    }
+    private IBinder mBinder = new ReceiverEnablerServiceBinder();
+    /**
+     * WatchdogServiceに接続した時の処理
+     */
+    private ServiceConnection mWatchdogServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+        }
 
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
     /**
      * バッテリー状態更新BroadcastをキャッチするためのBroadcastReceiver。
      */
-    public class BattStatusReceiver extends BroadcastReceiver {
-
+    private BroadcastReceiver mBatteryStatusReceived = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (intent != null) {
@@ -33,6 +42,36 @@ public class ReceiverEnablerService extends Service {
                     BattLevelCaptureService.startCaptureAction(context, battLevel);
                 }
             }
+        }
+    };
+
+    public ReceiverEnablerService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        registerReceiver(mBatteryStatusReceived, filter);
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        unregisterReceiver(mBatteryStatusReceived);
+    }
+
+    public class ReceiverEnablerServiceBinder extends Binder {
+        ReceiverEnablerService getService() {
+            return ReceiverEnablerService.this;
         }
     }
 }
--- mobile/src/main/java/net/formula97/miragewatch/WatchdogService.java
+++ mobile/src/main/java/net/formula97/miragewatch/WatchdogService.java
@@ -1,16 +1,84 @@
 package net.formula97.miragewatch;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.Binder;
 import android.os.IBinder;
+import android.text.TextUtils;
 
 public class WatchdogService extends Service {
+
+    public static final String ACTION_START_SHUTDOWN = WatchdogService.class.getName() + ".ACTION_START_SHUTDOWN";
+    private IBinder mBinder = new WatchdogServiceBinder();
+    /**
+     * ReceiverEnablerServiceに接続した時の処理
+     */
+    private ServiceConnection mReceiverEnablerServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+
+        }
+    };
+
     public WatchdogService() {
+    }
+
+    public static IntentFilter getTriggerFilter() {
+        return new IntentFilter(ACTION_START_SHUTDOWN);
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+    }
+
+    public class WatchdogServiceBinder extends Binder {
+        WatchdogService getService() {
+            return WatchdogService.this;
+        }
+    }
+
+    public class UnbindTriggerReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (!TextUtils.isEmpty(action) && action.equals(ACTION_START_SHUTDOWN)) {
+                // TODO 本体ServiceのUnbindを行う
+
+            }
+
+        }
     }
 }
