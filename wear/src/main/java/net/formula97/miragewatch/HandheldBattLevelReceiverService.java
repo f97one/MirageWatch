cf8a8daf8cbe4b92c3a8e25fd516d06cbb99c633
HAJIME Fukuna <f97one@gmail.com>
2016-01-04 00:10:34 +0900

    Google API Client経由で投げられたバッテリーレベルを受け取るためのスタブを追加。

---
+++ wear/src/main/java/net/formula97/miragewatch/HandheldBattLevelReceiverService.java
@@ -0,0 +1,14 @@
+package net.formula97.miragewatch;
+
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.WearableListenerService;
+
+/**
+ * Created by HAJIME on 2016/01/04.
+ */
+public class HandheldBattLevelReceiverService extends WearableListenerService {
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        super.onMessageReceived(messageEvent);
+    }
+}
