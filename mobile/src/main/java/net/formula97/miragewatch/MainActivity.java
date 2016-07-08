6437e6412af39cace36caa768aa36b9f38974d08
HAJIME Fukuna <f97one@gmail.com>
2016-07-08 23:49:24 +0900

    表示できそうなものがないので、とりあえず2条項BSDライセンスを表示する方向に修正。

--- mobile/src/main/java/net/formula97/miragewatch/MainActivity.java
+++ mobile/src/main/java/net/formula97/miragewatch/MainActivity.java
@@ -1,15 +1,16 @@
 package net.formula97.miragewatch;
 
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.os.BatteryManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -19,20 +20,23 @@ public class MainActivity extends AppCompatActivity {
     public static final String ACTION_SERVICE_STATE_UPDATED = MainActivity.class.getName() + ".ACTION_SERVICE_STATE_UPDATED";
     private final String sTag = this.getClass().getSimpleName();
 
-    @Bind(R.id.batteryLevel)
-    TextView batteryLevel;
-    @Bind(R.id.digitalModeBtn)
-    RadioButton digitalModeBtn;
-    @Bind(R.id.analogModeBtn)
-    RadioButton analogModeBtn;
+//    @Bind(R.id.batteryLevel)
+//    TextView batteryLevel;
+//    @Bind(R.id.digitalModeBtn)
+//    RadioButton digitalModeBtn;
+//    @Bind(R.id.analogModeBtn)
+//    RadioButton analogModeBtn;
 
     @Bind(R.id.adView)
     AdView adView;
 
+    @Bind(R.id.licenseHolder)
+    TextView licenseHolder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_beta);
 
         ButterKnife.bind(this);
 
@@ -48,14 +52,16 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         // バッテリー情報の収集
-        IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
-        Intent batt = registerReceiver(null, filter);
+//        IntentFilter filter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+//        Intent batt = registerReceiver(null, filter);
+//
+//        if (batt != null) {
+//            int level = batt.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
+//
+//            batteryLevel.setText(String.valueOf(level));
+//        }
 
-        if (batt != null) {
-            int level = batt.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
-
-            batteryLevel.setText(String.valueOf(level));
-        }
+        licenseHolder.setText(readFromAssets());
 
         adView.resume();
     }
@@ -65,5 +71,45 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
 
         adView.pause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        adView.destroy();
+    }
+
+    private String readFromAssets() {
+        InputStream inputStream = null;
+        BufferedReader bufferedReader = null;
+        String txt = "";
+
+        try {
+            inputStream = getAssets().open("LICENSE");
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            String str;
+            while ((str = bufferedReader.readLine()) != null) {
+                txt += str + "\n";
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return txt;
     }
 }
